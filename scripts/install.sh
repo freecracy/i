be50e2f30a814a6bd2c3f809697eb83b9479b827
freecracy <12114073+freecracy@users.noreply.github.com>
2019-09-22 16:21:40 +0000

    modify.install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-curl https://raw.githubusercontent.com/freecracy/l/master/i.sh > i
+curl https://raw.githubusercontent.com/freecracy/i/master/i.sh > i
 chmod +x i
 mv i /usr/local/bin/i
 
